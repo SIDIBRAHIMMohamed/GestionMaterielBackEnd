4611f1f55e1f90e566e8c4038dd677b48503016a
REDA TARGAOUI <76877668+RedaTargaoui@users.noreply.github.com>
2024-01-19 15:17:56 +0100

    Add files via upload

---
+++ src/main/java/com/project/GestionMaterielBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMaterielBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMaterielBackEndApplication.class, args);
+	}
+
+}
